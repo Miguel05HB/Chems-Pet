4a7c69e68a76e9bafc06684c7ef893d8a699462a
Miguel Angel <76132931@certus.edu.pe>
2022-06-14 08:24:17 -0500

    darkv2.0

--- dark.js
+++ dark.js
@@ -1,3 +1,20 @@
 document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+
+    /*GUARDANDO EL MODO NOCTURNO EN LOCALSTORAGE.*/
+
+    if(document.body.classList.contains('dark-theme')){
+        localStorage.setItem('dark-mode', 'true');
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    }
 });
+
+
+    //   OBTENER EL MODO ACUTAL DEL BOTON
+
+    if(localStorage.getItem('dark-mode') === 'true'){
+        document.getElementsByTagName('body')[0].classList.add("dark-theme");
+    }else{
+        document.getElementsByTagName('body')[0].classList.remove("dark-theme");
+    }
